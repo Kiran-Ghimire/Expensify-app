19d87305818ed71851e7ab793b6b87e899847fb0
N4secnT <54986538+N4secnT@users.noreply.github.com>
2020-10-09 14:23:11 +0545

    Fixing setTextFilter

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -69,7 +69,7 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
-    settextFilter: (text) => dispatch(setTextFilter(text)),
+    setTextFilter: (text) => dispatch(setTextFilter(text)),
     sortByDate: () => dispatch(sortByDate()),
     sortByAmount: () => dispatch(sortByAmount()),
     setStartDate: (startDate) => dispatch(setStartDate(startDate)),
